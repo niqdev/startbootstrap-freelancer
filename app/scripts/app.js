d9575ef71fb740b1e4b52400dfed5b3882d0f89a
niqdev <niqdev@gmail.com>
2015-09-16 08:05:04 +0200

    generated project structure with yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name startbootstrapFreelancerApp
+ * @description
+ * # startbootstrapFreelancerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('startbootstrapFreelancerApp', [
+    'ngAria',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name startbootstrapFreelancerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the startbootstrapFreelancerApp
+ */
+angular.module('startbootstrapFreelancerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
