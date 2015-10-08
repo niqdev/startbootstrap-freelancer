6f4d498d4cc8f93a205374bdd563fe5c785fa9ac
niqdev <niqdev@gmail.com>
2015-10-08 22:29:13 +0200

    add google-maps lib

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,8 +29,10 @@ module.exports = function(config) {
       'bower_components/angular-translate/angular-translate.js',
       'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
       'bower_components/lodash/lodash.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
+      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
