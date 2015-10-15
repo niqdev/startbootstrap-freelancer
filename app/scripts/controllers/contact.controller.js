c24da6386222a40e4829e58502f50e7edf336d46
niqdev <niqdev@gmail.com>
2015-10-15 07:19:02 +0200

    draft contact maps

--- app/scripts/controllers/contact.controller.js
+++ app/scripts/controllers/contact.controller.js
@@ -9,33 +9,39 @@
   function ContactController($log, $scope, homeService) {
 
     resetContact();
+    initButtons();
+    initMap();
 
-    $scope.btnSend = function() {
-      $scope.contactView.showInvalidFields = false;
+    function initButtons() {
+      $scope.btnSend = function() {
+        $scope.contactView.showInvalidFields = false;
 
-      if ($scope.contactForm.$valid) {
-        $scope.contactView.isBtnSendDisabled = true;
+        if ($scope.contactForm.$valid) {
+          $scope.contactView.isBtnSendDisabled = true;
+          sendMessage($scope.contactModel);
+        } else {
+          $log.error('INVALID');
+          $scope.contactView.showInvalidFields = true;
+          $scope.contactView.isBtnSendDisabled = false;
+        }
+      };
 
-        homeService.sendMessage($scope.contactModel).then(
-          function(response) {
-            $log.debug('SEND_MESSAGE: ' + JSON.stringify(response));
-            clearForm();
-          },
-          function(reason) {
-            $log.error('SEND_MESSAGE: ' + JSON.stringify(reason));
-          }
-        );
+      $scope.btnCancel = function() {
+        clearForm();
+      };
+    }
 
-      } else {
-        $log.error('INVALID');
-        $scope.contactView.showInvalidFields = true;
-        $scope.contactView.isBtnSendDisabled = false;
-      }
-    };
-
-    $scope.btnCancel = function() {
-      clearForm();
-    };
+    function sendMessage(data) {
+      homeService.sendMessage(data).then(
+        function(response) {
+          $log.debug('SEND_MESSAGE: ' + JSON.stringify(response));
+          clearForm();
+        },
+        function(reason) {
+          $log.error('SEND_MESSAGE: ' + JSON.stringify(reason));
+        }
+      );
+    }
 
     function resetContact() {
       $scope.contactModel = {};
@@ -48,6 +54,10 @@
     function clearForm() {
       resetContact();
       $scope.contactForm.$setPristine();
+    }
+
+    function initMap() {
+      $scope.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
     }
 
   }
